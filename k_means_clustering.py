2a37f0a033da6202738d6716d4c02467b8c1a279
Graham Eckel <grahameckel@gmail.com>
2020-11-16 08:23:09 -0500

    Significant performance improvement. Still room for improvement in random centroid assignment and n dimensial array algebra

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -1,43 +1,65 @@
 import random
 import numpy as np
+import pandas as pd
 
 #the mnist dataset is stored in the keras library, so we pull it from there
 from keras.datasets import mnist
 
+#assigning arrays
 (TrainX, TrainY), (TestX, TestY) = mnist.load_data()
 
-#representing each 28*28 greyscale image as a vector with size 784
+# Representing each 28*28 greyscale image as a vector with size 784
 TrainX = TrainX.reshape(len(TrainX),-1)
 TestX = TestX.reshape(len(TestX), -1)
 
-#Determine the number of clusters
+# Determine the number of clusters and iterations
 K = len(np.unique(TestY))
-iterations = 1
+iterations = 10
 
-def kmeanscluster(data, K):
-    #creating K random centroids using dimensions and elements of our test data
-    Centers = np.array([]).reshape(data.shape[1],0)
-    for i in range(K):
-        Centers = np.c_[Centers,data[random.randint(0,data.shape[0]-1)]]
+def kmeanscluster(data, K, P):
+    # Initializing an iterator, a distance (from the point to the centroid) array, and our cost arrays
+    rows = data.shape[0]
+    distance = np.random.rand(rows)
+    costs = np.random.rand(K)
     
-    #the magic
-    for i in range(iterations):
-        euclid=np.array([]).reshape(data.shape[0],0)
-        for k in range(K):
-            dist=np.sum((data-Centers[:,k])**2,axis=1)
-            euclid=np.c_[euclid,dist]
-        c=np.argmin(euclid,axis=1)+1
-        Cent={}
-        for k in range(K):
-            Cent[k+1]=np.array([]).reshape(data.shape[1],0)
-        for k in range(data.shape[0]):
-            Cent[c[k]]=np.c_[Cent[c[k]],data[k]]
-        for k in range(K):
-            Cent[k+1]=Cent[k+1].T
-        for k in range(K):
-            Centers[:,k]=np.mean(Cent[k+1],axis=0)
-        final=Cent    
-    return final
-      
-FinalCenters = kmeanscluster(TestX, K)
-print(FinalCenters)
+    # Creating K random centroids from pulled data and setting a random seed for reproducibility
+    random.seed(2020-11-14) #keep on when testing results
+    rand = np.array([random.randint(0, 10000) for k in range(0, K)])
+    centroids = data[rand,:]
+    
+    CostResult = []
+    CentroidResult = []
+    DataResult = []
+    
+    for i in range(0, P):
+        for i in range(0, rows):
+            #finding the nearest centre of every row in TrainX and assigning it to the nearest centroid
+            distance[i] = np.argmin([np.sum((centroids[k,]-data[i,])**2) for k in range(len(centroids))]) 
+    
+        #taking the mean of all vectors associated with each cluster, updating new centroids to this vector 
+        clustered = pd.DataFrame(data)   
+        clustered['centroid number'] = distance
+        means = clustered.groupby(['centroid number']).mean()
+        centroids = means.to_numpy()
+ 
+        DataResult.append(clustered)
+        #DataResult = np.array(ClusteredResult)    
+    
+        CentroidResult.append(centroids)
+        #CentroidResult = np.array(CentroidResult)   
+ 
+        #compute the sum of the squared distances from each data point to its centroid (cost)
+        for k in range(len(centroids)):  
+            cluster = np.array(clustered.loc[clustered['centroid number'] == k])
+            bycluster = cluster[:,:-1].copy()
+            costs[k] = np.sum((bycluster-centroids[k,])**2)
+        CostResult.append(sum(costs))
+        #CostResult = np.array(CentroidResult)
+     
+    return DataResult, CentroidResult, CostResult
+
+data, centroids, Jclust = kmeanscluster(TestX, K, iterations)
+
+IterList = range(0,K)
+for i, j in zip(Jclust, IterList):
+    print('Iteration {} - Cost: {}'.format(j,i))
